69f1a867d020f5cf8dffa780f25a3ecc76e6c86c
Pavel Korchagin <mortalis94@gmail.com>
2024-11-30 19:57:25 +0300

    feat: implement publish handler

--- internal/transport/http/v1/publish.go
+++ internal/transport/http/v1/publish.go
@@ -1,9 +1,45 @@
 package v1
 
 import (
+	"errors"
+	"io"
+	"log/slog"
 	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+
+	"github.com/Mort4lis/message-broker/internal/core"
 )
 
 func (h *Handler) Publish(w http.ResponseWriter, req *http.Request) {
-	_, _ = w, req
+	ps := httprouter.ParamsFromContext(req.Context())
+
+	queueName := ps.ByName("queue_name")
+	logger := h.logger.With(slog.String("queue_name", queueName))
+
+	queue, ok := h.reg.GetByName(queueName)
+	if !ok {
+		logger.Error("queue is not found")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	payload, err := io.ReadAll(req.Body)
+	if err != nil {
+		logger.Error("read body", slog.Any("error", err))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	if err = queue.Append(payload); err != nil {
+		logger.Error("append message to queue", slog.Any("error", err))
+		if errors.Is(err, core.ErrQueueOverflowed) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 }
